f54f9afe244c868d8ee41d170e22d00a8d0bddb9
bbha99 <branha99@gmail.com>
2023-01-29 18:06:12 -0800

    practice using data structure trees

--- vampire.js
+++ vampire.js
@@ -10,22 +10,35 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let currentVampire = this;
+    let numberOfVampires = 0;
+
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+
+    return numberOfVampires;
 
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+    return false;
   }
 
   /** Stretch **/
@@ -36,7 +49,39 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    
+    if (this.creator === vampire) {
+      return vampire;
+    }
 
+    if (this === vampire.creator) {
+      return this;
+    }
+
+    if (this === vampire) {
+      return this;
+    }
+
+    if (!this.creator) {
+      return this;
+    }
+
+    if (this.creator === vampire.creator) {
+      return this.creator;
+    }
+
+    let thisVampire = this;
+    let currentVampire = vampire;
+    while (thisVampire) {
+      while (currentVampire) {
+        if (thisVampire.creator === currentVampire.creator) {
+          return thisVampire.creator;
+        }
+        currentVampire = currentVampire.creator;
+      }
+      currentVampire = vampire;
+      thisVampire = thisVampire.creator;
+    }
   }
 }
 
